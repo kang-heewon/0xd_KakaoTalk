dd7d492c0ad17f070dca19081c3a771b9c759292
dev-ciel <admin@cieldev.com>
2018-02-24 22:06:26 +0900

    방장님 데이터 활용

--- 0xd.R
+++ 0xd.R
@@ -2,9 +2,13 @@
 #install.packages("rJava")
 #install.packages("memoise")
 #install.packages("KoNLP")
-#Sys.setenv(JAVA_HOME="C:/Program Files/Java/jre-9.0.4/")
-#library(KoNLP)
-#library(dplyr)
+#install.packages("wordcloud")
+Sys.setenv(JAVA_HOME="C:/Program Files/Java/jre-9.0.4/")
+library(KoNLP)
+library(wordcloud)
+library(RColorBrewer)
+library(dplyr)
+library(ggplot2)
 #useNIADic()
 txt <- readLines("0xd.txt")
 wordcount <- table(unlist(txt))
@@ -14,4 +18,16 @@ hehe <- filter(hehe, nchar(word) >= 2)
 top20 <- hehe %>%
   arrange(desc(freq)) %>%
   head(20)
-head(top20)
+#pal <-brewer.pal(8,"Dark2")
+#set.seed(1234)
+#wordcloud(words= hehe$word,
+#          freq = hehe$freq,
+#          min.freq = 2,
+#          max.word = 50,
+#          random.order = F,
+#          rot.per = .1,
+#          scale = c(3,0.3),
+#          colors = pal)
+name=top20$word
+freq=top20$freq
+ggplot(data =top20,aes(x=reorder(name,-freq),y=freq))+geom_col()
